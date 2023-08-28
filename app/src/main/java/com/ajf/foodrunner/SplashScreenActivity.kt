81efe5bebd101ef81526234eb55c9d6010258048
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-08-28 05:02:14 +0400

    > Login Screen completed.

--- app/src/main/java/com/ajf/foodrunner/SplashScreenActivity.kt
+++ app/src/main/java/com/ajf/foodrunner/SplashScreenActivity.kt
@@ -1,11 +1,19 @@
 package com.ajf.foodrunner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }, 2000)
     }
 }
